cf85a6cb9d5f675a0ceb2e6e807f77803d777632
FatyGpe0 <fatimag1818inf@gmail.com>
2023-09-15 18:22:10 -0600

    Actualizacion boton de registrar

--- src/routers/index.js
+++ src/routers/index.js
@@ -11,4 +11,23 @@ router.get('/', async (req, res)=>{
     });
 }); //el get obtiene datos
 
+
+router.post('/add', async (req, res)=>{
+    const valor = new Cliente(req.body);
+    console.log(req.body);
+    await valor.save();
+    res.redirect('/');
+});
+
+
+router.get('/del/:id', async (req, res)=>{
+    const {id}=req.params;
+    await Cliente.findByIdAndRemove(id);
+    res.redirect('/');
+})
+
+
+//al presional el boton de eliminar se abra una ventana modal para que se confirme la eliminación
+//<a href="/del/_id"
+// el link que va a ejectar la aplicacion debe ir en el modal en la opcion de confirmar
 module.exports = router;
