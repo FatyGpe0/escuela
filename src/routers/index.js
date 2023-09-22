f22e26d60ed136043aa211f8128793fe872f2f41
FatyGpe0 <fatimag1818inf@gmail.com>
2023-09-22 17:23:23 -0600

    Actualizacion inicial del boton actualizar

--- src/routers/index.js
+++ src/routers/index.js
@@ -20,9 +20,15 @@ router.post('/add', async (req, res)=>{
 });
 
 
-router.get('/del/:id', async (req, res)=>{
+router.get('/del/:id', async (req, res)=>{   //Eliminar
     const {id}=req.params;
-    await Cliente.findByIdAndRemove(id);
+    await Cliente.findByIdAndRemove(id);   //Actualizar
+    res.redirect('/');
+})
+
+router.get('/upd/:id', async (req, res)=>{
+    const {id}=req.params;
+    await Cliente.findByIdAndUpdate(id);
     res.redirect('/');
 })
 
@@ -31,3 +37,8 @@ module.exports = router;
 //al presional el boton de eliminar se abra una ventana modal para que se confirme la eliminación
 //<a href="/del/_id"
 // el link que va a ejectar la aplicacion debe ir en el modal en la opcion de confirmar
+
+//TAREA
+//agregar boton de actualizar al lado de eliminar y de registrar, para que una ves que se presione al lado de
+//eliminar se pongan los elementos del registro seleccionado en los campos de arriba, los edito y al darle al boton 
+//de actualizar al lado de registrar se actualicen los valores en la tabla.... Expresss
